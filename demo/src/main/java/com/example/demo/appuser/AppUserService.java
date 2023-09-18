14b24d3b5132c7895da6d60a30b6a82d503bfbe4
Jason Lin <jasonlin@Jasons-MacBook-Air.local>
2023-09-18 16:01:31 -0400

    added initial files to project

---
+++ demo/src/main/java/com/example/demo/appuser/AppUserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.appuser;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/appuser/StudentReport.java
@@ -0,0 +1,4 @@
+package com.example.demo.appuser;
+
+public enum StudentReport {
+}
